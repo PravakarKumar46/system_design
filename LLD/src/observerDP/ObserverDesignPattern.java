48c6c3a39a3c41cc4cc1d04656482c8c1290dac2
pravakar <pravakark46@gmail.com>
2025-06-22 00:53:14 +0530

    Initial commit

---
+++ LLD/src/observerDP/ObserverDesignPattern.java
@@ -0,0 +1,7 @@
+package observerDP;
+
+public class ObserverDesignPattern {
+    public static void main(String[] args) {
+        System.out.println("observer design pattern...");
+    }
+}
---
+++ LLD/src/strategyDP/StrategyDesignPattern.java
@@ -0,0 +1,7 @@
+package strategyDP;
+
+public class StrategyDesignPattern {
+    public static void main(String[] args) {
+        System.out.println("strategy design patter...");
+    }
+}
